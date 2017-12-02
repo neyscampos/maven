1e586a83b85a301ff480f260dad3558325385e59
Joao Santana <wozefacu@cartelera.org>
2017-12-02 11:05:50 -0200

    criação de projeto com maven

--- src/main/java/entity/Cliente.java
+++ src/main/java/entity/Cliente.java
@@ -2,6 +2,28 @@ package entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
 public class Cliente implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idCliente;
+	@Column(length = 50)
+	private String nome;
+	@Column(length = 50, unique=true)
+	private String email;
+
+	public Cliente() {
+
+	}
 }
